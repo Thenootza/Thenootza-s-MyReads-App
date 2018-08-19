ddcb016def9cd287d534ca56bdf4679767eb2c4c
Daniela Cosmina Weigel <daniela.c.weigel@gmail.com>
2018-08-19 13:58:43 +0300

    Fixed the display of the books

--- src/MyBooks.js
+++ src/MyBooks.js
@@ -16,32 +16,37 @@ class MyBooks extends Component {
         <div className="bookshelf">
           <div className="list-books-content">
             <div className="bookshelf-books">
-
               <div className="books-grid">
                 {books.filter((books) => books.shelf === "currentlyReading").map((books) =>
                   <MyList  key={books.id} books={books} shelfChanger={shelfChanger}/>
 
                 )}
-                <h2 className="bookshelf-title">Currently Reading</h2>
               </div>
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              </div>
+            </div>
 
-
+            <div className="bookshelf">
               <div className="bookshelf-books">
-                {books.filter((books) => books.shelf === "wantToRead").map((books) =>
-                  <MyList  key={books.id} books={books} shelfChanger={shelfChanger}/>
+                <div className="books-grid">
+                  {books.filter((books) => books.shelf === "wantToRead").map((books) =>
+                    <MyList  key={books.id} books={books} shelfChanger={shelfChanger}/>
 
-                )}
+                  )}
+                </div>
                 <h2 className="bookshelf-title">Want To Read</h2>
               </div>
+            </div>
 
-
+            <div className="bookshelf">
               <div className="bookshelf-books">
+                <div className="books-grid">
                 {books.filter((books) => books.shelf === "read").map((books) =>
                   <MyList  key={books.id} books={books} shelfChanger={shelfChanger}/>
 
                 )}
-              <h2 className="bookshelf-title">Read</h2>
               </div>
+              <h2 className="bookshelf-title">Read</h2>
             </div>
           </div>
 
